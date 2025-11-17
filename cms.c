fe35bc67cb2e4003b1cea4baca364497eaa10cbd
limkaihin <108354887+limkaihin@users.noreply.github.com>
2025-11-17 15:46:54 +0800

    Update plagiarism policy declaration wording

--- cms.c
+++ cms.c
@@ -69,18 +69,18 @@ static int autosave_on = 0;
 
 static void print_declaration(void) {
     printf("\nDeclaration\n");
-    printf("SIT’s policy on copying does not allow the students to copy source code as well as assessment solutions\n");
-    printf("from another person AI or other places. It is the students’ responsibility to guarantee that their\n");
+    printf("SIT's policy on copying does not allow the students to copy source code as well as assessment solutions\n");
+    printf("from another person AI or other places. It is the students' responsibility to guarantee that their\n");
     printf("assessment solutions are their own work. Meanwhile, the students must also ensure that their work is\n");
     printf("not accessible by others. Where such plagiarism is detected, both of the assessments involved will\n");
     printf("receive ZERO mark.\n\n");
     printf("We hereby declare that:\n");
-    printf("• We fully understand and agree to the abovementioned plagiarism policy.\n");
-    printf("• We did not copy any code from others or from other places.\n");
-    printf("• We did not share our codes with others or upload to any other places for public access and will not do that in the future.\n");
-    printf("• We agree that our project will receive Zero mark if there is any plagiarism detected.\n");
-    printf("• We agree that we will not disclose any information or material of the group project to others or upload to any other places for public access.\n");
-    printf("• We agree that we did not copy any code directly from AI generated sources\n\n");
+    printf("- We fully understand and agree to the abovementioned plagiarism policy.\n");
+    printf("- We did not copy any code from others or from other places.\n");
+    printf("- We did not share our codes with others or upload to any other places for public access and will not do that in the future.\n");
+    printf("- We agree that our project will receive Zero mark if there is any plagiarism detected.\n");
+    printf("- We agree that we will not disclose any information or material of the group project to others or upload to any other places for public access.\n");
+    printf("- We agree that we did not copy any code directly from AI generated sources\n\n");
     printf("Declared by: Group Name: P10-09\n");
     printf("Team members:\n");
     printf("1. David Yong Jing Xiang\n2. Amal Nadiy\n3. Lim Kai Hin\n4. Liaw Jun De\n");
